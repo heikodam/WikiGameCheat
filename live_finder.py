ac3edde90257a0347e924bc1674381a2e0510dcc
[Heiko Damaske] <[heiko.damaske@code.berlin]>
2019-03-06 18:22:12 +0100

    momorize the path added for 2 degrees

--- live_finder.py
+++ live_finder.py
@@ -9,14 +9,21 @@ def is_link_in_page(page_to_check, end_link):
 
 
 start_link = "/wiki/What_the_Funny"
-end_link = "/wiki/I%27m_Gonna_Git_You_Suckaasd"
+end_link = "/wiki/Tha_Truth,_Pt._2"
+path = [start_link]
 
 links_on_start_page = getAllUrl(start_link)
+
 
 if is_link_in_page(start_link, end_link):
         print("Found the Page")
 
+
 for current_page in links_on_start_page:
     if is_link_in_page(current_page, end_link):
         print("Found the Page")
+        path.append(current_page)
         break
+
+path.append(end_link)
+print(path)
